6e9c2e2c254554c125e27405673f95a228e4105e
yeliang <yeliang@dachagui.com>
2019-06-14 21:20:01 +0800

    add

--- edu-release.sh
+++ edu-release.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
-scp -P 54188 -r /Users/leafrontye/h5-piaoniu-server/* root@111.231.78.195:/data/www/h5-piaoniu-server
 scp -P 54188 -r /Users/leafrontye/h5-piaoniu-static/target/index.html root@111.231.78.195:/data/www/h5-piaoniu-server/views
