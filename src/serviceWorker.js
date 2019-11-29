961498f38097fe0d51bfdbe9794ce8579aa69f35
Liang Ye <yeliang@dachagui.com>
2019-11-29 20:32:36 +0800

    udpate serviceWorker.js

--- src/serviceWorker.js
+++ src/serviceWorker.js
@@ -1,13 +1,12 @@
 
 // 当前缓存版本的唯一标识符，用当前时间代替
-var VERSION = "201911291747"
+var VERSION = "201911292032"
 
 // 当前缓存白名单，在新脚本的 install 事件里将使用白名单里的 key
 
 var cacheFileList = [
-  "staticPath/static/js/h5-framework/1.0.0/index.js",
   "staticPath/static/css/h5-framework/1.0.0/index.css",
-  "staticPath/static/??js/vue/2.6.10/index.js,js/polyfill/1.0.0/index.js"
+  "staticPath/static/??js/vue/2.6.10/index.js,js/h5-framework/1.0.0/index.js"
 ]
 self.addEventListener('install', (event) =>  {
   // 等待所有资源缓存完成时，才可以进行下一步
