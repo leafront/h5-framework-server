8097bc526ed43bc40b3ac5fd5673c1de5d338bd1
Liang Ye <yeliang@dachagui.com>
2019-12-05 11:45:43 +0800

    update router/user.js

--- router/user.js
+++ router/user.js
@@ -1,11 +1,11 @@
 const router = require('koa-router')()
 router.get('/personal', async (ctx, next) => {
-  await ctx.render('user/personal',{
+  await ctx.render('user/personal', {
   })
 })
 
 router.get('/login', async (ctx, next) => {
-  await ctx.render('user/login',{
+  await ctx.render('user/login', {
    
   })
 })
