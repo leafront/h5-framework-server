1c845323254157a4cfeef7dd76e053fb6e5fcb37
Liang Ye <yeliang@dachagui.com>
2019-12-18 11:41:05 +0800

    update luck draw

--- src/pages/luck/draw.vue
+++ src/pages/luck/draw.vue
@@ -1,20 +1,33 @@
 <template>  
   <div class="page-view" id="app">
     <div class="scroll-view-wrapper">
-      <div class="draw-outer">
-        <div class="draw-inner">
-          <div class="draw-inner-wrapper">
-            <div class="draw-inner-item draw-inner-item1">
-              <span class="cfff fs30">一等奖</span>
-            </div>
-            <div class="draw-inner-item draw-inner-item2"></div>
-            <div class="draw-inner-item draw-inner-item3"></div>  
-            <div class="draw-inner-item draw-inner-item4"></div> 
-            <div class="draw-inner-item draw-inner-item5"></div>
-            <div class="draw-inner-item draw-inner-item6"></div>     
+      <div class="draw-outer-wrapper" ref="drawOuter">
+        <div class="draw-luck-start" @click="startLuckDraw"></div>
+        <div class="draw-outer" :class="{'active': isLuckDraw}">
+          <div class="draw-inner">
+            <div class="draw-inner-wrapper">
+              <div class="draw-inner-item draw-inner-item1">
+                <span class="cfff fs30">一等奖</span>
+              </div>
+              <div class="draw-inner-item draw-inner-item2">
+                 <span class="cfff fs30">二等奖</span>
+              </div>
+              <div class="draw-inner-item draw-inner-item3">
+                 <span class="cfff fs30">三等奖</span>
+              </div>  
+              <div class="draw-inner-item draw-inner-item4">
+                 <span class="cfff fs30">四等奖</span>
+              </div> 
+              <div class="draw-inner-item draw-inner-item5">
+                 <span class="cfff fs30">五等奖</span>
+              </div>
+              <div class="draw-inner-item draw-inner-item6">
+                 <span class="cfff fs30">六等奖</span>
+              </div>     
+            </div>  
           </div>  
-        </div>  
-      </div>  
+        </div>
+      </div>    
     </div>
   </div>
 </template>  
@@ -23,14 +36,40 @@
   body{
   background: #fff;
 }
-.draw-outer{
+.draw-outer-wrapper{
+  position: relative;
+  height: 7.1rem;
   margin: 2rem .2rem;
+}
+.draw-luck-start{
+  background: url(imgPath/static/img/luck_draw_btn.png?v=201912171828) no-repeat;
+  background-size: 100% auto;
+  width: 1.79rem;
+  height: 2.19rem;
+  position: absolute;
+  left: 50%;
+  top: 50%;
+  margin-left: -.895rem;
+  margin-top: -1.095rem;
+  transform: none;
+  transition: none;
+  z-index: 100;
+}
+.draw-outer{
   height: 7.1rem;
   background: url(imgPath/static/img/draw_outer.png?v=201912161609) no-repeat;
   background-size: 100% auto;
   padding: .4rem;
   display: flex;
   align-items: center;
+  transform: rotate(0deg);
+  position: absolute;
+  left: 0;
+  top: 0;
+  &.active{
+    transform: rotate(3330deg);
+    transition: transform 6s linear;
+  }
 }
 .draw-inner{
   width: 100%;
@@ -45,6 +84,7 @@
   overflow: hidden;
   width: 6rem;
   height: 6rem;
+  transform: rotate(0deg);
 }
 .draw-inner-item{
   position: absolute;
@@ -54,9 +94,7 @@
   right: 0;
   transform-origin: 0% 100%;
   span{
-    transform: skewY(30deg) rotate(30deg);
-    transform-origin: 50% 50%;
-    margin-top: 2rem;
+    transform: skewY(30deg) rotate(30deg) translate(1.2rem, .7rem);
     position: absolute;
   }
 }
@@ -90,14 +128,29 @@
   export default {
     data () {
       return {
-
+        isLuckDraw: false,
+        isStopAnimate: false
       }
     },
-    created () {
-
+    mounted () {
+      this.$refs.drawOuter.addEventListener('transitionstart', () => {
+        this.isStopAnimate = true
+      })
+      this.$refs.drawOuter.addEventListener('transitionend', () => {
+        this.isStopAnimate = false
+      })
     },
     methods: {
-     
+      startLuckDraw () {
+        if (this.isStopAnimate) {
+          return
+        }
+        this.isStopAnimate = true
+        this.isLuckDraw = false
+        setTimeout(() => {
+          this.isLuckDraw = true
+        }, 100)
+      }
     }
   }
 </script>
